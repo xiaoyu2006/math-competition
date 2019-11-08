bb8cee48b4fda40604440fdc8e542e382df8eb6b
xiaoyu2006 <1783250036@qq.com>
2019-11-08 11:57:06 +0800

    Extends Bootstrap

--- competition/views.py
+++ competition/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Answer, Competition, Record, User, Problem
+
 # Create your views here.
 def index(request):
-    return HttpResponse("INDEX PAGE")
+    context = {
+        'comps': Competition.objects.order_by('-end_time'),
+    }
+    return render(request, 'competition/index.html', context=context)
