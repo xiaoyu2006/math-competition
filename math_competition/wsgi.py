8855056c4fe823bf31f77465ce15dc1d096ff04c
xiaoyu2006 <1783250036@qq.com>
2019-11-10 01:24:51 +0800

    Heroku

--- math_competition/wsgi.py
+++ math_competition/wsgi.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'math_competition.settings')
 
-application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
