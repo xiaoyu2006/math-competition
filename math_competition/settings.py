d4f6d4dd19a9dab3494ccd5b7319a353278450a9
xiaoyu2006 <1783250036@qq.com>
2019-11-10 01:08:28 +0800

    Settings.py

--- math_competition/settings.py
+++ math_competition/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4b&2&^_rhnv7am3hw=)@b6r3e6^=(ipx%$m99!6n(#ya4efg(+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -122,3 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_URL = '/login/'
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
