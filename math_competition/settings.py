7bb7f06b474a49b4b272c95023dc667da825edc4
xiaoyu2006 <1783250036@qq.com>
2019-11-10 01:45:26 +0800

    ???

--- math_competition/settings.py
+++ math_competition/settings.py
@@ -130,9 +130,4 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'competition/static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
